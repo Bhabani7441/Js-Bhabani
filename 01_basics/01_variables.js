40ca25880dc70927e636b702208b66624b7659ec
Bhabani7441 <bsbhabani18@gmail.com>
2024-06-30 04:38:25 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountID = 12223454
+let accountEmaiL = "mIKU12@gmail.com"
+var accountPassword = "12345"
+accountCity = "Karanjia"
+let accountState;
+
+// accountID = 2 // not allowed
+console.log(accountID);
+
+accountEmail = "rahul23@gmail.com"
+console.log(accountEmail)
+
+accountPassword = "2132312"
+console.log(accountPassword)
+
+accountCity = "Bhubaneswer"
+console.log(accountCity)
+
+
+console.table([accountID,accountEmaiL, accountPassword,accountCity,accountState])
